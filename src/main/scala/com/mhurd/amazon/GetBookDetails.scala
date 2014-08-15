2ecf1e06d4bbea57ff3abb90b0d9413b5dbaae86
mhurd <mjwhurd@gmail.com>
2014-08-15 13:27:26 +0100

    Added usage

--- src/main/scala/com/mhurd/amazon/GetBookDetails.scala
+++ src/main/scala/com/mhurd/amazon/GetBookDetails.scala
@@ -6,18 +6,22 @@ import BookJsonProtocol._
 object GetBookDetails {
 
   def main(args: Array[String]) {
-    val isbn = args(0)
-    val accessKey = args(1)
-    val secretKey = args(2)
-    val assocTag = args(3)
-    val client = AmazonClient(accessKey, secretKey, assocTag)
-    val xmlData = client.findByIsbn(isbn)
-    val bookOption = Book.fromAmazonXml(isbn, xmlData)
-    bookOption match {
-      case None => println("Book not found!")
-      case Some(book) => println(book.toJson.prettyPrint)
+    if (args.length < 4) {
+      println("Usage: <isbn> <access_key> <secret_key> <associate_tag>")
+    } else {
+      val isbn = args(0)
+      val accessKey = args(1)
+      val secretKey = args(2)
+      val assocTag = args(3)
+      val client = AmazonClient(accessKey, secretKey, assocTag)
+      val xmlData = client.findByIsbn(isbn)
+      val bookOption = Book.fromAmazonXml(isbn, xmlData)
+      bookOption match {
+        case None => println("Book not found!")
+        case Some(book) => println(book.toJson.prettyPrint)
+      }
+      client.close()
     }
-    client.close()
   }
 
 }
